4654ff7a8ec87431a3c145a799694167395fe98f
Jonathan Foster <jxf9099@rit.edu>
2018-01-10 16:21:06 -0500

    fresh fountainjs copy

---
+++ bin/angularjs-scripts.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const gulp = require('gulp');
+require('../gulpfile'); // import gulp tasks
+const args = process.argv.slice(2);
+
+const scriptIndex = args.findIndex(
+  x => x === 'build'
+    || x === 'serve'
+    || x === 'serve:dist'
+    || x === 'test'
+    || x === 'test:auto'
+);
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+
+switch (script) {
+  case 'build':
+  case 'serve':
+  case 'serve:dist':
+  case 'test':
+  case 'test:auto': {
+    gulp.task(script)();
+    break;
+  }
+  default:
+    console.log('Unknown script "' + script + '".');
+    console.log('Perhaps you need to update angularjs-scripts?');
+    console.log(
+      'Email jonathan.foster@eagledream.com for more information.'
+    );
+    break;
+}
+
