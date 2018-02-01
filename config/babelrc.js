b968ee2f828cd228175a5fe69977f4e829a09c02
Jonathan Foster <jxf9099@rit.edu>
2018-02-01 11:50:51 -0500

    clean up babel config

--- config/babelrc.js
+++ config/babelrc.js
@@ -8,9 +8,6 @@ module.exports = {
   plugins: [
     [require.resolve('babel-plugin-angularjs-annotate'), {
       explicitOnly: true
-    }],
-    require.resolve('babel-plugin-transform-runtime'),
-    require.resolve('babel-plugin-syntax-async-functions'),
-    require.resolve('babel-plugin-syntax-class-properties')
+    }]
   ]
 };
